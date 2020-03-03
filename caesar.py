7448158830bc2e0a772da98ad265e12a990b2212
ap-ple <55882424+ap-ple@users.noreply.github.com>
2020-03-02 21:06:55 -0500

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,20 @@
+from stuff import *
+from string import ascii_lowercase
+cls()
+
+def caesar(text, shift, encrypt, alpha):
+	cipher = alpha[shift:] + alpha[:shift]
+	key = {alpha[i]: cipher[i] for i in range(len(alpha))} if encrypt else {cipher[i]: alpha[i] for i in range(len(alpha))}
+	return ''.join([l if l.lower() not in alpha else (key[l.lower()].upper() if l.upper() == l else key[l]) for l in text])
+	
+while True:
+	encrypt = False if 'd' in input('Decrypt or Encrypt: ') else True
+	shift = num_input('Shift (leave blank for all shifts): ')
+	text = input('Text: ')
+	alpha = ascii_lowercase
+	if shift > 0:
+		dialogue(caesar(text, shift, encrypt, alpha))
+	else:
+		for s in range(1, 26):
+			print(f'{s}) {caesar(text, s, encrypt, alpha)}')
+		dialogue()
