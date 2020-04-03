34ea3622566e3f4f6c0e353407f186fe144205cb
ap-ple <55882424+ap-ple@users.noreply.github.com>
2020-04-03 02:06:19 -0400

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,15 +1,17 @@
-from stuff import *
+from stuff import clear, dialogue, num_input
 from string import ascii_lowercase
-cls()
+clear()
 
 def caesar(text, shift, encrypt, alpha):
 	cipher = alpha[shift:] + alpha[:shift]
 	key = {alpha[i]: cipher[i] for i in range(len(alpha))} if encrypt else {cipher[i]: alpha[i] for i in range(len(alpha))}
-	return ''.join([l if l.lower() not in alpha else (key[l.lower()].upper() if l.upper() == l else key[l]) for l in text])
+	return ''.join(l if l.lower() not in alpha else (key[l.lower()].upper() if l.upper() == l else key[l]) for l in text)
 	
 while True:
-	encrypt = False if 'd' in input('Decrypt or Encrypt: ') else True
+	encrypt = False if 'd' in input('Decrypt or Encrypt: ').lower() else True
+	print('Encrypt' if encrypt else 'Decrypt')
 	shift = num_input('Shift (leave blank for all shifts): ')
+	print('All shifts' if shift == 0 else shift)
 	text = input('Text: ')
 	alpha = ascii_lowercase
 	if shift > 0:
